386f58a1ef4af0fb467f4a5abfde7e13caa9884c
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-09-09 16:08:43 -0700

    Update arachne.clj
    - modify arachne startup prompt
    - make additional functions available

--- src/agents/arachne.clj
+++ src/agents/arachne.clj
@@ -15,10 +15,12 @@
 
 (def arachne-agent-config
   {:name "Arachne Biomedical Data Fabric Agent"
+   ;; :provider :openai ;; (@u :model-provider)
+   ;; :model "gpt-4o"
    :provider :anthropic ;; (@u :model-provider)
    :model "claude-3-5-sonnet-20241022"
    :role (str
-          "You are a data management agent who specializes in reviewing diverse data templates based on the CCDI standard and translating them to a desired common standard called **GDC**. "
+          "You are a data management agent who specializes in reviewing diverse data templates based on the MC2 standard and translating them to a desired common standard called **GDC**. "
           "Your most common workflow consists of obtaining from the user the paths to one or more CSV templates of entity data that they need to transform to a target set of templates in the GDC standard. "
           "For each CSV file with records of some entity type, you can use the 'read_file' tool to extract and see the entity data records. "
           ;; "Then you can use the tool load_into_working_graph to put the entity data into an OLAP knowledge graph, which is similar to using a data warehouse for data transformations. " 
@@ -27,7 +29,7 @@
           "You may retrieve the list of potential target templates in the GDC standard. Note that the inputs may not have a 1:1 match to the GDC templates, so not all GDC templates are output targets, only the relevant ones. "
           "Once you have determined which GDC templates to output and how to translate the data sufficiently, either submit the csv data directly or use the mapping/transform specification schema (below), whichever is better. The output must contain/specify all columns in the target template!\n"
           (slurp (io/resource "specs/map_spec.json")))
-   :tools #{:find-matching-attribute :get-attribute-meta :get-template-meta :list-standard-templates :summarize-csv}})
+   :tools #{:load-prebuilt-graph :find-matching-attribute :get-attribute-meta :get-template-meta :list-standard-templates :summarize-csv :submit-data}})
 
 (def Arachne (agent/create-agent arachne-agent-config))
 
